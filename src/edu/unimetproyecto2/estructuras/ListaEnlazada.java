38fc347f04a2f8fe6c2f3b54acdeba0f12180439
ChantalPT <chantyspinto@gmail.com>
2026-03-14 19:23:27 -0400

    Fix: Se agregó el puntero ultimo y el método remover a las listas enlazadas

--- src/edu/unimetproyecto2/estructuras/ListaEnlazada.java
+++ src/edu/unimetproyecto2/estructuras/ListaEnlazada.java
@@ -7,34 +7,65 @@ package edu.unimetproyecto2.estructuras;
 /**
  *
  * @author jesus alejandro
+ * @param <T>
  */
 public class ListaEnlazada<T> {
     private Nodo<T> cabeza;
+    private Nodo<T> ultimo;
     private int tamano;
+    
 
     public ListaEnlazada() {
         this.cabeza = null;
+        this.ultimo = null;
         this.tamano = 0;
+    }
+    
+    public boolean estaVacia() {
+        return cabeza == null;
     }
 
     // Método para insertar al final (útil para la cola de procesos)
     public void insertar(T dato) {
         Nodo<T> nuevoNodo = new Nodo<>(dato);
-        if (cabeza == null) {
+        if (estaVacia()) {
             cabeza = nuevoNodo;
+            ultimo = nuevoNodo;
         } else {
-            Nodo<T> temporal = cabeza;
-            while (temporal.getSiguiente() != null) {
-                temporal = temporal.getSiguiente();
-            }
-            temporal.setSiguiente(nuevoNodo);
+            ultimo.setSiguiente(nuevoNodo);
+            ultimo = nuevoNodo; 
         }
         tamano++;
+    }
+    
+    public void remover(T dato) {
+        if (estaVacia()) return;
+
+        if (cabeza.getDato().equals(dato)) {
+            cabeza = cabeza.getSiguiente();
+            tamano--;
+            if (cabeza == null) {
+                ultimo = null;
+            }
+            return;
+        }
+
+        Nodo<T> actual = cabeza;
+        while (actual.getSiguiente() != null && !actual.getSiguiente().getDato().equals(dato)) {
+            actual = actual.getSiguiente();
+        }
+
+        if (actual.getSiguiente() != null) {
+            if (actual.getSiguiente() == ultimo) {
+                ultimo = actual;
+            }
+            actual.setSiguiente(actual.getSiguiente().getSiguiente());
+            tamano--;
+        }
     }
 
     public int getTamano() { return tamano; }
     
-    // Método para obtener un elemento por índice (para el JTable)
     public T obtener(int indice) {
         if (indice < 0 || indice >= tamano) return null;
         Nodo<T> temporal = cabeza;
@@ -43,4 +74,6 @@ public class ListaEnlazada<T> {
         }
         return temporal.getDato();
     }
+    
+    
 }
