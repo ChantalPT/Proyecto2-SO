c7176bf0bfef3a941c896baf35070abf029f79ab
ChantalPT <chantyspinto@gmail.com>
2026-03-15 18:41:44 -0400

    ???

--- src/edu/unimetproyecto2/controlador/ManejadorJournaling.java
+++ src/edu/unimetproyecto2/controlador/ManejadorJournaling.java
@@ -4,10 +4,40 @@
  */
 package edu.unimetproyecto2.controlador;
 
+import edu.unimetproyecto2.estructuras.ListaEnlazada;
+
 /**
  *
  * @author pinto
  */
 public class ManejadorJournaling {
+    private ListaEnlazada transacciones;
+    
+    public ManejadorJournaling() {
+        this.transacciones = new ListaEnlazada();
+    }
+    
+    public RegistroJournaling registrar(String operacion, String nombreEntrada) {
+        RegistroJournaling nuevaTx = new RegistroJournaling(operacion, nombreEntrada);
+        transacciones.insertar(nuevaTx);
+        return nuevaTx;
+    }
+    
+    public ListaEnlazada getHistorial() {
+        return transacciones;
+    }
+    
+    public ListaEnlazada obtenerPendientes() {
+        ListaEnlazada fallidas = new ListaEnlazada();
+        int total = transacciones.getTamano();
+        
+        for (int i = 0; i < total; i++) {
+            RegistroJournaling tx = (RegistroJournaling) transacciones.obtener(i);
+            if (tx.getEstado().equals("PENDIENTE")) {
+                fallidas.insertar(tx);
+            }
+        }
+        return fallidas;
+    }
     
 }
