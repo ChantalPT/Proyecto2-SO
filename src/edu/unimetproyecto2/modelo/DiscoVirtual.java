fa55691419841dc9b3adb89d7c569f4e55f0d066
ChantalPT <chantyspinto@gmail.com>
2026-03-15 11:36:44 -0400

    fix: En DiscoVirtual se agrego metodos getEspacioLibre y BuscarBloqueLibre, ademas de asignarArchivo

--- src/edu/unimetproyecto2/modelo/DiscoVirtual.java
+++ src/edu/unimetproyecto2/modelo/DiscoVirtual.java
@@ -4,6 +4,8 @@
  */
 package edu.unimetproyecto2.modelo;
 
+import java.awt.Color;
+
 /**
  *
  * @author jesus alejandro
@@ -21,6 +23,15 @@ public class DiscoVirtual {
             bloques[i] = new Bloque(i);
         }
     }
+    
+    //Cant. de bloques libres total
+    public int getEspacioLibre() {
+        int libres = 0;
+        for (int i = 0; i < tamanoTotal; i++) {
+            if (!bloques[i].isOcupado()) libres++;
+        }
+        return libres;
+    }
 
     // Buscar el primer bloque libre disponible
     public int buscarBloqueLibre() {
@@ -32,7 +43,61 @@ public class DiscoVirtual {
         return -1; // Disco lleno
     }
     
+    public boolean asignarArchivo(Archivo archivo) {
+        if (getEspacioLibre() < archivo.getTamanoBloques()) {
+            return false; // False pn no hay espacio suficiente en el disco
+        }
+
+        int bloquesNecesarios = archivo.getTamanoBloques();
+        int bloqueAnterior = -1;
+
+        for (int i = 0; i < bloquesNecesarios; i++) {
+            int indiceLibre = buscarBloqueLibre();
+            Bloque b = bloques[indiceLibre];
+            
+            //Llenar el bloque con los datos del archivo
+            b.setOcupado(true);
+            b.setNombreArchivo(archivo.getNombre());
+            b.setColorArchivo(archivo.getColor());
+            archivo.agregarBloque(indiceLibre); //Guardar en la lista interna del archivo
+
+            if (bloqueAnterior != -1) { //Se encadena al siguiente bloque
+                bloques[bloqueAnterior].setSiguienteBloque(indiceLibre);
+            }
+
+            bloqueAnterior = indiceLibre; 
+        }
+        return true; 
+    }
+    
+    public void liberarArchivo(Archivo archivo) {
+        //Buscar primer bloque desde la lista del archivo
+        if (archivo.getBloquesAsignados().estaVacia()) 
+            return;
+        
+        int indiceActual = archivo.getBloquesAsignados().obtener(0); 
+        
+        // Recorremos la cadena de bloques en el disco y limpiarlos
+        while (indiceActual != -1) {
+            Bloque b = bloques[indiceActual];
+            int siguiente = b.getSiguienteBloque(); //Guardar su apuntador
+            
+            // Formatear el bloque
+            b.setOcupado(false);
+            b.setNombreArchivo("LIBRE");
+            b.setColorArchivo(Color.WHITE);
+            b.setSiguienteBloque(-1);
+            
+            indiceActual = siguiente; 
+        }
+    }
+    
+    //Getters
     public Bloque getBloque(int id) {
         return bloques[id];
+    }
+    
+    public int getTamanoTotal() {
+        return tamanoTotal;
     }
 }
